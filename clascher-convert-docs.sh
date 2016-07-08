36dc08077d66c1af513910cf2b699807b6e721f8
clasch <clasch@live.unc.edu>
2016-07-08 12:58:22 -0400

    converted anth paper from md to docx, html, odt, and pdf

--- clascher-convert-docs.sh
+++ clascher-convert-docs.sh
@@ -1,13 +1,19 @@
-
+#!/bin/bash
 
 # Declare input argument as a variable
+var=assignment-3
 
 # Convert markdown to HTML
+pandoc -o $var.html $var.md
 
 # Convert markdown to DOCX
+pandoc -o $var.docx $var.md
 
 # Convert markdown to ODT
+pandoc -o $var.odt $var.md
 
 # Convert markdown to PDF
+pandoc -o $var.pdf $var.md
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted $var to HTML, DOCX, ODT, PDF"
